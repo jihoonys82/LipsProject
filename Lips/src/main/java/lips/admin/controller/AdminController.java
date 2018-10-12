7ae29ac1ffd150d9e53f2fb4b517aaafdff75326
JuMoni531 <ccolog531@gmail.com>
2018-10-12 13:49:07 +0900

    update jh

--- Lips/src/main/java/lips/admin/controller/AdminController.java
+++ Lips/src/main/java/lips/admin/controller/AdminController.java
@@ -1,20 +1,18 @@
 package lips.admin.controller;
-
-import org.slf4j.Logger;
+ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
+ @Controller
 public class AdminController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-	
-	@RequestMapping(value="/admin/main")
-	public String main() {
-		logger.info("메인 페이지");
-		
-		return "admin/main";
-	}
-	
+   
+   private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+   
+   @RequestMapping(value="/admin/main")
+   public String main() {
+      logger.info("메인 페이지");
+      
+      return "admin/main";
+   }
+   
 }

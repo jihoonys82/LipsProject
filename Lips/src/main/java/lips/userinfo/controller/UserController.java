43abcf43e653896945cbc4185809cdc0c1d91432
lack9503 <lack9503@gmail.com>
2018-10-17 12:04:31 +0900

    update YJ

--- Lips/src/main/java/lips/userinfo/controller/UserController.java
+++ Lips/src/main/java/lips/userinfo/controller/UserController.java
@@ -1,26 +1,14 @@
 package lips.userinfo.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import lips.userinfo.dto.User;
-import lips.userinfo.service.UserService;
 
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
-	
-
-	@Autowired UserService userService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);  
 	
@@ -28,49 +16,4 @@ public class UserController {
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public void login() {}
 	
-	
-	
-	
-	
-//	@RequestMapping(value="/login", method=RequestMethod.POST)
-//	public ModelAndView loginProc(User user, String useCookie, HttpSession session, HttpServletResponse response) {
-//		ModelAndView mav = new ModelAndView();
-//		logger.info(useCookie);
-//		User loginUser = userService.login(user);
-//		if(loginUser != null) {
-//			logger.info(loginUser.toString());
-//			loginUser.setPw(null);
-//			session.setAttribute("user", loginUser);
-//			session.setAttribute("login", true);
-//			if(useCookie != null) {
-//				Cookie loginCookie = new Cookie("loginCookie", session.getId());
-//				userService.addLoginServer(user, session.getId());
-//				loginCookie.setMaxAge(60*60*24*7);
-//				response.addCookie(loginCookie);
-//			}
-//			userService.addLoginServer(loginUser, session.getId());
-//			mav.setViewName("redirect:/main");
-//		}else{
-//			mav.setViewName("user/login");
-//		}
-//		return mav;
-//	}
-//	
-//	
-//	@RequestMapping(value="logout")
-//	public String logout(HttpSession session, HttpServletResponse response) {
-//		session.invalidate();
-//		Cookie cookie = new Cookie("loginCookie","");
-//		cookie.setMaxAge(0);
-//		response.addCookie(cookie);
-//		return "redirect:/main";
-//	}
-//	@RequestMapping(value="logoutauto")
-//	public String logoutAuto(HttpSession session,HttpServletResponse response) {	
-//		Cookie cookie = new Cookie("loginCookie","");
-//		cookie.setMaxAge(0);
-//		response.addCookie(cookie);
-//		session.invalidate();
-//		return "user/logoutauto";
-//	}
 }
--- Lips/src/main/java/lips/userinfo/dao/UserDao.java
+++ Lips/src/main/java/lips/userinfo/dao/UserDao.java
@@ -1,57 +1,14 @@
 package lips.userinfo.dao;
 
-import java.util.HashMap;
-
 import lips.userinfo.dto.User;
 
 public interface UserDao {
-	
-	/**
-	 * @param inputData_login
-	 * @return User
-	 * 입력데이터를 검증하여 유저객체를 반환한다.
-	 */
-	public User selUserByIdPw(User user);
+
 	/**
 	 * @param selCookie_login
 	 * @return User
 	 * 쿠키값으로 찾은 아이디로 조회하여 유저객체를 반환한다.
 	 */
 	public User selUserById(String userId);
-	/**
-	 * @param sessionId_cookie
-	 * @return User
-	 * 쿠키값을 조회하여 아이디를 반환한다.
-	 */
-	public User selIdBySId(String sessionId);
-	/**
-	 * @param sessionId_cookie
-	 * @return int
-	 * 로그인서버에 세션정보와 아이디가 존재하는지 확인한다.
-	 */
-	public int selLoginCntBySId(String sessionId);
-	/**
-	 * @param userId
-	 * @return int
-	 * 로그인서버에 세션정보와 아이디가 존재하는지 확인한다.
-	 */
-	public int selLoginCntByUId(String userId);
-	
-	/**
-	 * @param map
-	 * 로그인서버에 유저정보를 추가한다.
-	 */
-	public void inUserLoginServer(HashMap<String,Object> map);
-	/**
-	 * @param map
-	 * 로그인서버에 유저정보를 갱신한다.
-	 */
-	public void upUserLoginServer(HashMap<String,Object> map);
-	
-	/**
-	 *  
-	 * @return int
-	 * 접속중인 유저수를 반환한다.
-	 */
-	public int selActiveUserCnt();
+
 }
